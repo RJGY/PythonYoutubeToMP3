7a0c28a9f67faa7ef5c4ec12de7a7e26f44182ba
Reese <rjgy2yu@gmail.com>
2020-09-05 01:05:39 +1000

    Made the code modular
    Signed-off-by: Reese <rjgy2yu@gmail.com>

--- converter.py
+++ converter.py
@@ -1,25 +1,11 @@
-from downloader import *
 from pydub import AudioSegment
-import pydub
 import os
 
 
-def convertm4atomp3(videofile, conversionFolder):
-    logging.debug("Start of Conversion")
+def converttomp3(videofile, conversionfolder=None, relative=True):
     mp3name = os.path.splitext(os.path.basename(videofile))[0] + ".mp3"
     song = AudioSegment.from_file(videofile, "webm")
-    song.export(os.getcwd() + conversionFolder + mp3name, format="mp3")
-    logging.debug("End of Conversion")
-
-
-if __name__ == '__main__':
-    # Test Now
-    urlList = playlist(examplePlaylist)
-    for video in urlList:
-        # make a try catch whatever thingo
-        try:
-            convertm4atomp3(downloadvideoaudio(video))
-        except pydub.exceptions.CouldntDecodeError:
-            convertm4atomp3(downloadvideoaudio2(video))
-
-    logging.debug("DONE")
+    if relative:
+        song.export(os.getcwd() + conversionfolder + mp3name, format="mp3")
+    else:
+        song.export(conversionfolder + mp3name, format="mp3")
--- downloader.py
+++ downloader.py
@@ -1,55 +1,31 @@
 import pafy
-import logging
 import os
 from pytube import YouTube
 
-logging.basicConfig(level=logging.DEBUG, format=" %(asctime)s -  %(levelname)s -  %(message)s")
-logging.debug("Start of Program.")
-
-downloadfolder = '\\webms\\'
-exampleVideo = r"https://www.youtube.com/watch?v=HS2idHXAKUU"
-examplePlaylist = r"https://www.youtube.com/playlist?list=PLUDyUa7vgsQlEST5MYSqTmc03U0Mr_Ihc"
-startingIndex = 564
-pafy.set_api_key("AIzaSyCQyk6dS1nomkmGlPuK-zJc9CjGg6ziWFA")
-
 
 # this uses pafy to download
-def downloadvideoaudio(videoURL):
-    logging.debug("Start of downloadvideo")
-    audiostream = pafy.new(videoURL).getbestaudio() # YouTube(videoURL).streams.get_by_itag('251')
+def downloadvideoaudio(videoURL, downloadfolder="\\tempDownload\\"):
+    audiostream = pafy.new(videoURL).getbestaudio()
     # Download
     audiostream.download(os.getcwd() + downloadfolder + audiostream.title.replace("/", "_").replace("'", "").replace("|", "") + "." + audiostream.extension, True)
-    logging.debug(audiostream.title)
-    logging.debug("End of downloadvideo")
     return os.getcwd() + downloadfolder + audiostream.title.replace("/", "_").replace("'", "").replace("|", "") + "." + audiostream.extension
 
 
 # this uses pytube to download
-def downloadvideoaudio2(videoURL):
-    logging.debug("Start of downloadvideo2")
+def downloadvideoaudio2(videoURL, downloadfolder="\\tempDownload\\"):
     audiostream = YouTube(videoURL).streams.get_by_itag('251')
     # Download
     audiostream.download(os.getcwd() + downloadfolder)
-    logging.debug("End of downloadvideo2")
     return os.getcwd() + downloadfolder + audiostream.title.replace(".", "").replace(",", "").replace("'", "") + ".webm"
 
 
-def playlist(playlistURL):
+def playlist(playlistURL, startingindex, endingindex):
     playlistURLs = []
-    logging.debug("Start of playlist")
-    playlist = pafy.get_playlist2(examplePlaylist)
-    for i in range(len(playlist)):
-        logging.debug("we are at index " + str(i))
+    playlist = pafy.get_playlist2(playlistURL)
+    if endingindex > len(playlist):
+        endingindex = len(playlist)
+    if startingindex < 0:
+        startingindex = 0
+    for i in range(startingindex, endingindex):
         playlistURLs.append("https://www.youtube.com/watch?v=" + playlist[i].videoid)
-    logging.debug("End of playlist")
     return playlistURLs
-
-
-if __name__ == '__main__':
-    # logging.debug(downloadvideoaudio(exampleVideo))
-    logging.debug(YouTube(exampleVideo).streams)
-    #playlist(examplePlaylist)
-
-
-logging.debug("End of Program.")
-
