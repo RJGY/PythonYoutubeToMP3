658f96729be9b50bfc647faac7593b2a06521528
Reese <rjgy2yu@gmail.com>
2020-09-08 02:07:42 +1000

    Added todo for next method.

--- pathcheck.py
+++ pathcheck.py
@@ -14,9 +14,16 @@ def pathexists(dirpath, relative):
             exit()
 
 
+# TODO - cache clear should only run when temp download has hit 1GB of space.
 def clearcache(dirpath, relative):
     if relative:
         dirpath = os.getcwd() + dirpath
     for file in os.listdir(dirpath):
         if os.path.isfile(dirpath + file):
             os.remove(dirpath + file)
+
+
+def checkcache(dirpath, relative):
+    if relative:
+        dirpath = os.getcwd() + dirpath
+
